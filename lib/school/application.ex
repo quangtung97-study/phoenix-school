d4eae2daeeef7984fe554d52b64637cfb51c7956
quangtung97 <quangtung29121997@gmail.com>
2018-11-02 11:22:18 +0700

    Init

---
+++ lib/school/application.ex
@@ -0,0 +1,31 @@
+defmodule School.Application do
+  use Application
+
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Start the Ecto repository
+      supervisor(School.Repo, []),
+      # Start the endpoint when the application starts
+      supervisor(SchoolWeb.Endpoint, []),
+      # Start your own worker by calling: School.Worker.start_link(arg1, arg2, arg3)
+      # worker(School.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: School.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    SchoolWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
